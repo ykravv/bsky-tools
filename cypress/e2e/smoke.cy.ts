a3283a408645638dc593ece57ace2f2f4656b491
Yevhenii Kravchenko <yevheniikr@wix.com>
2023-08-20 14:54:44 +0300

    Fix cy command

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,6 +1,6 @@
 describe("smoke tests", () => {
   it("should open login form", () => {
-    cy.visitAndCheck("/");
+    cy.visit("/");
 
     cy.findByText("Sign in to your account").should("exist");
   });
